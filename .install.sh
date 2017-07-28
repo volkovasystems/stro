ffa05109054de83ed65d5b9814ba31d195fbf09f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-28 23:49:02 +0800

    Added .install.sh.

---
+++ .install.sh
@@ -0,0 +1,34 @@
+yarn add \
+babel-runtime \
+ehm \
+harden \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+electron \
+gulp \
+gulp-babel \
+gulp-changed \
+gulp-debug \
+gulp-plumber \
+gulp-rename \
+gulp-replace \
+gulp-sourcemaps \
+mocha \
+should \
+source-map-loader \
+wdio-json-reporter \
+wdio-mocha-framework \
+wdio-selenium-standalone-service \
+webdriverio \
+webpack \
+yargs \
+--dev && \
+bower install \
+ehm \
+harden \
+--save \
+--force-latest
